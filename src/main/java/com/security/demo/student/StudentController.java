6f9b60725f6e5177bf5a8a0dc0c9780db2813578
saidozgat <said.ozgat@consultant.turkcell.com.tr>
2022-07-22 14:45:11 +0300

    +api created

---
+++ src/main/java/com/security/demo/student/StudentController.java
@@ -0,0 +1,28 @@
+package com.security.demo.student;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/students")
+public class StudentController {
+
+    private static final List<Student> STUDENTS = Arrays.asList(
+            new Student(1,"Said Ozgat"),
+            new Student(2,"Ibrahim Yildirim"),
+            new Student(2,"Taner Sener")
+    );
+
+    @GetMapping(path = "{studentId}")
+    public Student getStudent(@PathVariable("studentId") Integer studentId){
+        return STUDENTS.stream()
+                .filter(student -> studentId.equals(student.getStudentId()))
+                .findFirst()
+                .orElseThrow(() -> new IllegalStateException("Student " + studentId + " does not exists"));
+    }
+}
